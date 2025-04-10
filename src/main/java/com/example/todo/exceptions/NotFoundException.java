d5783a388d7d7f6750e85dca03bd3924fb105c78
DiegoBas17 <167855623+DiegoBas17@users.noreply.github.com>
2025-04-10 13:15:34 +0200

    gestione errori

---
+++ src/main/java/com/example/todo/exceptions/NotFoundException.java
@@ -0,0 +1,13 @@
+package com.example.todo.exceptions;
+
+import java.util.UUID;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+    public NotFoundException(UUID id) {
+        super("Non è stato possibile trovare il record con id " + id);
+    }
+}
