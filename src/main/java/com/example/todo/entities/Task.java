6c8845ce59056df0b4f43c3677013160e6946e00
DiegoBas17 <167855623+DiegoBas17@users.noreply.github.com>
2025-04-10 13:15:09 +0200

    creazione entities

---
+++ src/main/java/com/example/todo/entities/Task.java
@@ -0,0 +1,26 @@
+package com.example.todo.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private boolean completed;
+    private LocalDateTime createdAt = LocalDateTime.now();
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
---
+++ src/main/java/com/example/todo/entities/User.java
@@ -0,0 +1,23 @@
+package com.example.todo.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    @OneToMany(mappedBy = "user")
+    private List<Task> tasks;
+}
